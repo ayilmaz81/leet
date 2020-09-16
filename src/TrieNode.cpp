f0267786b5687f66c2957420314578623a40fd53
Ahmet Yilmaz <ayilmaz81@gmail.com>
2020-09-16 13:54:10 +0100

    removed excess code.

--- src/TrieNode.cpp
+++ src/TrieNode.cpp
@@ -21,32 +21,3 @@ void TrieNode::removeWord(const std::string&word, int index){
     auto c = word[index] - 'a';
     if (children[c] != nullptr) children[c]->removeWord(word,index+1);
 }
-
-class MostTraded{
-    int numberOfShares;
-    unordered_map<string,int> shares;
-    set<pair<int,string>> topK;
-    public:
-    
-    MostTraded(int n):numberOfShares(n){}
-    
-    void addTrade(const string& entity, int number){
-        auto leastTraded = topK.begin();
-        if (leastTraded != topK.end() && leastTraded->first < shares[entity] + number){
-            auto it = topK.find({shares[entity], entity});
-            if (it != topK.end()){
-                topK.erase(it);
-            }
-            topK.insert({shares[entity], entity});
-            if (topK.size() > numberOfShares){
-                topK.erase(topK.begin());
-            }
-        }
-        shares[entity] += number;
-
-        
-    }
-    void printMostTraded(){
-        
-    }
-}
